5b96919221782f61e1c88e2f2d26141fd7bd3ba9
acodingwitch <70824102+somecodingwitch@users.noreply.github.com>
2024-08-28 13:01:12 -0300

    maybe focus on make_bdm_matrix_base

--- bin/main.ml
+++ bin/main.ml
@@ -1,2 +1,14 @@
 
+let rec map f l =
+  match l with
+  | [] -> []
+  | h :: t -> f h :: map f t;;
+  
+let make_bdm_matrix_base size = 
+  let num_range = List.init size (fun i -> i + size) in
+  let char_range = map (fun _ -> '.') num_range in
+  let block_range = char_range in
+  block_range
+;;
+
 let () = Printf.printf "Hello world";;
--- lib/utils.ml
+++
@@ -1,7 +0,0 @@
-let range from until =
-  List.init (until - from) (fun i -> i + from)
-
-let rec map f l =
-  match l with
-  | [] -> []
-  | h :: t -> f h :: map f t;;
